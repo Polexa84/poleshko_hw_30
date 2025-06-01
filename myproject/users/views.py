9980a020bd5318b0a7b4d8a1a89d833335902887
polexa84 <polexa84@gmail.com>
2025-06-01 22:57:22 +0300

    Реалезовано следующие задание Реализуйте CRUD для пользователей, в том числе регистрацию пользователей, настройте в проекте использование JWT-авторизации и закройте каждый эндпоинт авторизацией.

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,11 +1,37 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Payment
 from .serializers import PaymentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-from .filters import PaymentFilter  # Импортируем наш фильтр
+from .filters import PaymentFilter
+from django.conf import settings
+from django.apps import apps  # Импортируем apps
+User = apps.get_model(settings.AUTH_USER_MODEL)  # Получаем модель пользователя
+from .serializers import UserSerializer, RegisterSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 class PaymentListAPIView(generics.ListAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    filter_backends = [DjangoFilterBackend]  # Добавляем бэкенд фильтрации
-    filterset_class = PaymentFilter  # Указываем класс фильтра
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PaymentFilter
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
