4690e6e4396365cd751922fc000059d2e2490831
polexa84 <polexa84@gmail.com>
2025-06-01 23:13:46 +0300

    Реалезовано следующие задание
    Заведите группу модераторов и опишите для нее права работы с любыми уроками и курсами, но без возможности их удалять и создавать новые. Заложите функционал такой проверки в контроллеры.

--- myproject/users/views.py
+++ myproject/users/views.py
@@ -4,10 +4,12 @@ from .serializers import PaymentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import PaymentFilter
 from django.conf import settings
-from django.apps import apps  # Импортируем apps
-User = apps.get_model(settings.AUTH_USER_MODEL)  # Получаем модель пользователя
+from django.apps import apps
+from .permissions import IsModerator  # Импортируем permission
 from .serializers import UserSerializer, RegisterSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+User = apps.get_model(settings.AUTH_USER_MODEL)  # Получаем модель пользователя
 
 
 class PaymentListAPIView(generics.ListAPIView):
@@ -17,15 +19,31 @@ class PaymentListAPIView(generics.ListAPIView):
     filterset_class = PaymentFilter
 
 
-class UserList(generics.ListCreateAPIView):
+class UserList(generics.ListAPIView):  # Только для суперпользователей
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserDetail(generics.RetrieveAPIView):  # Авторизованный пользователь видит только свой профиль
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+class UserUpdate(generics.UpdateAPIView):  # Авторизованный пользователь может редактировать только свой профиль
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+class UserDelete(generics.DestroyAPIView):  # Только для суперпользователей
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
+    permission_classes = [permissions.IsAdminUser]
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -35,3 +53,23 @@ class RegisterView(generics.CreateAPIView):
 
 class MyTokenObtainPairView(TokenObtainPairView):
     permission_classes = (permissions.AllowAny,)
+
+class PaymentUpdateAPIView(generics.UpdateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsModerator|permissions.IsAdminUser]
+
+class PaymentDeleteAPIView(generics.DestroyAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class PaymentCreateAPIView(generics.CreateAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class PaymentDetailAPIView(generics.RetrieveAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = [permissions.IsAuthenticated, IsModerator|permissions.IsAdminUser]
