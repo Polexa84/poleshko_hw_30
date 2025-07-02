1432de3ea082141e8e09cd2192bab6a931a32032
polexa84 <polexa84@gmail.com>
2025-07-02 14:21:19 +0300

    Тесты возвращаю постепенно найти в ком ошибка

--- myproject/lms/tests.py
+++ myproject/lms/tests.py
@@ -1,13 +1,9 @@
-'''
-# lms/tests.py
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
-from .models import Course, Lesson, Subscription
+from lms.models import Course, Lesson, Subscription
 
 User = get_user_model()
 
-# ... остальной код ...
-'''
