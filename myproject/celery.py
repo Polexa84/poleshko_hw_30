0e0ac54b5e376f5b7e4237c20a37bf6ebc742102
polexa84 <polexa84@gmail.com>
2025-07-09 18:49:48 +0300

    Обновлен селари

--- myproject/celery.py
+++ myproject/celery.py
@@ -1,27 +1,28 @@
 import os
 from celery import Celery
 from django.conf import settings
-#from celery.schedules import crontab  # Уберем, так как используем django-celery-beat
+from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.config.settings')  # Обратите внимание на путь!
 
 app = Celery('myproject')
 
-# Load celery config from settings.py
+# 1. Настройка из Django settings
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Optional: Timezone setting.  Can also be done in settings.py
-# app.conf.timezone = settings.TIME_ZONE
+# 2. Дополнительные настройки
+app.conf.update(
+    timezone=settings.TIME_ZONE,
+    beat_schedule={
+        'deactivate-inactive-users': {
+            'task': 'users.tasks.deactivate_inactive_users',
+            'schedule': crontab(minute=0, hour=3),
+        },
+    }
+)
 
-# Autodiscover tasks - use this if your tasks are in Django apps
-from django.apps import apps
-app.autodiscover_tasks(lambda: [app_config.name for app_config in apps.get_app_configs()])
-
-# Optional: Add schedule with django-celery-beat.  Remove this section if not using beat
-# from celery.schedules import crontab
-# app.conf.beat_schedule = {
-#     'deactivate-inactive-users': {
-#         'task': 'users.tasks.deactivate_inactive_users',
-#         'schedule': crontab(minute=0, hour=3),
-#     },
-# }
+# 3. Автодискавери задач
+app.autodiscover_tasks(
+    lambda: [app for app in settings.INSTALLED_APPS
+           if not app.startswith('django.')]
+)
