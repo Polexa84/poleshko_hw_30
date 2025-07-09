95d279fc662c2f5805b91568e97c400cdafa436d
polexa84 <polexa84@gmail.com>
2025-07-09 17:42:20 +0300

    Обновлены настройки

--- myproject/config/settings.py
+++ myproject/config/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',  # Добавляем django_filters
     'rest_framework',  # Добавляем DRF
-    'myproject.users.apps.UsersConfig',  # Добавляем приложение users
+    'users',  # Добавляем приложение users
     'lms',  # Добавляем приложение lms
     'drf_spectacular',
     'django_celery_beat',
